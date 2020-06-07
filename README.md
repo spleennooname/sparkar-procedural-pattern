2d13a9bc4a8be294fe4527d5060ab85a926f37ec
spleennooname <spleen666@gmail.com>
2020-06-07 21:47:48 +0200

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+![](https://img.shields.io/badge/sdk-v85-green-+) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+# sparkar-procedural-pattern
+> an example of how to derive a procedural texture pattern in spark-ar via script.
+
+![preview-1](./preview-1.jpg)
+
+![preview-2](./preview-2.jpg)
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
+/**
+ * Procedural texture with
+ * v85+
+ */
 
+// modules
 const R = require('Reactive')
 const Time = require('Time')
 const M = require('Materials')
@@ -23,12 +28,12 @@ const rotate2d = (uv, angle) => {
   return uv
 }
 
-//luma
 const luma = sample => {
   const l = R.dot(R.pack4(0.299, 0.587, 0.114, 1), R.pack4(sample.x, sample.y, sample.z, sample.w))
   return R.pack4(l, l, l, sample.w)
 }
 
+// main
 Promise.all([
   M.findFirst('cameraMat'),
   T.findFirst('cameraTexture0'),
@@ -39,13 +44,13 @@ Promise.all([
     const cameraMaterial = assets[0]
     const cameraSignal = assets[1].signal
     const matSignal = assets[2].signal
-
+    // uv
     const uv = Shaders.fragmentStage(Shaders.vertexAttribute({
-      'variableName': Shaders.VertexAttribute.TEX_COORDS
+      variableName: Shaders.VertexAttribute.TEX_COORDS
     }))
 
     const time = Time.ms.mul(0.01)
-
+    // pattern
     const levels = 5.7
     const angle = Math.PI / levels
     const spacing = 0.014
@@ -55,10 +60,10 @@ Promise.all([
     const width = 0.005
     const dist = 0.10
 
-    const guilloche = (color, uv, time) => {
+    const pattern = (color, uv, time) => {
       let result = R.pack4(1, 1, 1, 1)
       const sample = luma(Shaders.textureSampler(color, uv))
-      for (let i = 0, nuv, wave, x, y, line, waves, fq; i < levels; i += 1.0) {
+      for (let i = 0, nuv, wave, x, y, line, waves; i < levels; i += 1.0) {
         nuv = rotate2d(uv, angle + angle * i)
         wave = R.sin(nuv.x.mul(frequency)).mul(height)
         x = wave.sum((spacing * 0.85))
@@ -76,12 +81,12 @@ Promise.all([
     const frequency = R.abs(R.sin(time.mul(0.01)).mul(20))
 
     const render = (signal, uv, time) => {
-      const guillocheSignal = guilloche(signal, uv, time)
+      const patternSignal = pattern(signal, uv, time)
       const displacementSignal = luma(Shaders.composition(cameraSignal, uv))
       const displacementOffset = R.mul(displacementSignal.y, displacementFactor)
       cameraMaterial.setTextureSlot(
         Shaders.DefaultMaterialTextures.DIFFUSE,
-        Shaders.composition(guillocheSignal, uv.add(displacementOffset))
+        Shaders.composition(patternSignal, uv.add(displacementOffset))
       )
     }
 
